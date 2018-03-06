e7e168dc3e95b6d5b69497587fb46b9dfffb4b0d
unfa <unfa00@gmail.com>
2018-03-06 12:20:00 +0100

    Separate parts and threads, better temporary file naming
    Now the script takes 3 arguments:
    1. Kdenlive render script
    2. Amount of parts to split the job into
    3. Amount of threads to be rendered in parallel
    
    I've made this change becasue I had system crashes when rendering big projects where 5 hours of CPU time were lost at 90% of completion. Now I can just split the whole project into 64 parts, and have 6 rendered in parallel at all times - it won't bog down my system, but I have much less probablility of loosing all progress on a system crash. Sometimes Kdenline rendering crashes too. Having more smaller parts helps so mitigate the problem.
    
    Also - all temporary files are now prefixed with kmr-, so a simple 'rm kmr-*' will clean it up.
    
    Details are in the source code comments.

--- kdenlive-multirender.sh
+++ kdenlive-multirender.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # kdenlive-multirender.sh | Multi-threaded video rendering for Kdenlive
-# Version 0.0.1
-# Created by unfa 2017-12-13
+# Version 0.0.2
+# Created by unfa, 2017-12-13, 2018-03-06
+
+### File naming: all tempporary files created by Kdenlive-Multirender are prefixed with 'kmr-'. So after the fact you can clean it up wiht 'rm kmr-*' command (unless you've choosen to name other files in this fashion).
 
 # Getting input
 
 INPUT=$1 # The first passed argument has to be a Kdenlive render script (for example "Project_001.sh")
-PARTS=$2 # The second passed parameter is the number of threads to use.
+PARTS=$2 # The second passed argument is the number of parts to split the rendering job into.  High values (like 64) will help mnimize data loss in case of a system crash when rendering a big project. Values smaller than THREADS make no sense.
+THREADS=$3 # The third passed argument is the amount of rendering threads to be running at any given time in parallell. Advised values are between 2 and 6. It's recommended to start with a smaller value and see how much memory and CPU time is consumed with a tool like htop. Running too many threads may starve the system for memory and as a result - take it down or progress extremely slowly, which defeats the purpose of this script.
 
-# processing the information
+## functions
 
-IN=$(grep -o " in=[0-9]* "  "$INPUT" | cut -d= -f2 | xargs) # Get the first frame for the project
-OUT=$(grep -o " out=[0-9]* "  "$INPUT" | cut -d= -f2 | xargs) # Get the last frame for the project
-PATHRAW=$(grep -o "TARGET_0=.*"  "$INPUT") # Identify the target file path
-PATHFILE=$(dirname "${PATHRAW}" | sed -e 's/TARGET_0="file:\/\///g' ) # Get the clean file path
-TARGET=$(grep -o "TARGET_0=.*"  "$INPUT" | rev | cut -d/ -f1 | rev | cut -d'"' -f1) # Get the output video file for this project
-
-#ffmpeg can't read urlencoded paths
+# ffmpeg can't read urlencoded paths
 urldecode() {
     # urldecode <string>
 
@@ -25,11 +22,21 @@ urldecode() {
     printf '%b' "${url_encoded//%/\\x}"
 }
 
+# extracting needed information from the input Kdenlive rendering script
+
+IN=$(grep -o " in=[0-9]* "  "$INPUT" | cut -d= -f2 | xargs) # Get the first frame for the project
+OUT=$(grep -o " out=[0-9]* "  "$INPUT" | cut -d= -f2 | xargs) # Get the last frame for the project
+PATHRAW=$(grep -o "TARGET_0=.*"  "$INPUT") # Identify the target file path
+PATHFILE=$(dirname "${PATHRAW}" | sed -e 's/TARGET_0="file:\/\///g' ) # Get the clean file path
+TARGET=$(grep -o "TARGET_0=.*"  "$INPUT" | rev | cut -d/ -f1 | rev | cut -d'"' -f1) # Get the output video file for this project
+
 echo "IN=$IN OUT=$OUT" # verify we got it right
 
-rm list.txt # clear the list.txt file
+rm kmr-list.txt # clear the kmr-list.txt file
 
-for i in $(seq -w 01 $PARTS); do # for each thread...
+### Prepare individual rendering scripts for each part
+
+for i in $(seq -w 01 $PARTS); do # for each part...
     
     PART=$i
     IN2=$(echo "(($OUT - $IN) / $PARTS ) * ($PART - 1)" | bc) # calculate the thread start frame
@@ -42,16 +49,27 @@ for i in $(seq -w 01 $PARTS); do # for each thread...
     TB=$(echo $TARGET | cut -d. -f1) # slice out the basename of the output file
     TE=$(echo $TARGET | cut -d. -f2) # slice out the filename extension
     
-    TARGET2="$TB-$i.$TE" # compose a filename for the thread output
+    TARGET2="kmr-$TB-$i.$TE" # compose a filename for the thread output
     
     echo "PARTS=$PARTS PART=$PART IN=$IN2 OUT=$OUT2" # print it out for verification
     echo 
     
-    sed -e "s/in=$IN/in=$IN2/" "$INPUT" | sed -e "s/out=$OUT/out=$OUT2/" | sed -e "s/$TARGET/$TARGET2/" > "$PART.sh" # replace the IN, OUT and TARGET information in the source Kdenlicve render script and write that to a new shell script
+    sed -e "s/in=$IN/in=$IN2/" "$INPUT" | sed -e "s/out=$OUT/out=$OUT2/" | sed -e "s/$TARGET/$TARGET2/" > "kmr-$PART.sh" # replace the IN, OUT and TARGET information in the source Kdenlicve render script and write that to a new shell script
     
-    echo "file '$(urldecode $PATHFILE/$TARGET2)'" >> list.txt # add a line to out concatenation list.txt file for ffmpeg to use later
+    echo "file '$(urldecode $PATHFILE/$TARGET2)'" >> kmr-list.txt # add a line to out concatenation list.txt file for ffmpeg to use later
+done
+
+### exeute the script, maintining no more than $THREADS  concurrent processes
+
+for i in $(seq -w 01 $PARTS); do # for each part...
     
-    bash "$PART.sh" & # run the newly created script in the background
+    while [[ $(pgrep -f "bash kmr-" -c) > $(( $THREADS - 1 )) ]]; do # sleeping for so long as there's $THREADS or more processes running
+        echo "waiting for a thread to finish..."
+        sleep 15s
+    done
+    
+    echo "Starting thread $i"
+    bash "kmr-$PART.sh" & # run another scrpt in the background
     echo
 done
 
@@ -59,6 +77,6 @@ wait # wait until all rendering threads finish
 
 echo "Concatenating files..."
 
-ffmpeg -f concat -safe 0 -i list.txt -c copy "$(urldecode $PATHFILE/$TARGET)" # merge the individual files into one
+ffmpeg -f concat -safe 0 -i kmr-list.txt -c copy "$(urldecode $PATHFILE/$TARGET)" # merge the individual files into one
 
 echo "All done!"
